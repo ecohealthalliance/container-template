bae001a5fcebafde3be2ba2cbb9cbf9d4996b264
Noam Ross <ross@ecohealthalliance.org>
2022-10-28 14:07:01 -0400

    renaming

--- use-template.R
+++ use-template.R
@@ -38,13 +38,15 @@ message("Modifying template to use '", project_name, "' as name")
 for (pfile in projfiles) {
   # Rename any files with the template name to the project name
   if(grepl("container-template", pfile)) {
-    file.rename(pfile, gsub("container-template", project_name, pfile, fixed = TRUE))
+    new_name <- gsub("container-template", project_name, pfile, fixed = TRUE)
+    file.rename(pfile, new_name)
+    pfile <- new_name
   }
   # Replace any text of placeholders with the project name
   lines <- readLines(pfile)
   updated_lines <- gsub("container-template", project_name, lines, fixed = TRUE)
-  cat(paste(updated_lines, collapse = "\n"), pfile)
+  cat(paste(updated_lines, collapse = "\n"), file = pfile)
 }
 
 setwd(template_directory)
-system2("Rscript initialize-template.R")
+system("Rscript initialize-template.R")
